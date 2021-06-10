e1a2b577d22b6c3f945157f2b0170f1e707f4ac5
zaeem ahmed <zahmed.bscs18seecs@seecs.edu.pk>
2021-06-11 00:04:01 +0500

    update

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import os
 from flask import Flask, flash, request, redirect, url_for, session, send_file
 from werkzeug.utils import secure_filename
 from flask_cors import CORS, cross_origin
-import logging
 from PIL import ImageFile, Image
 from tensorflow.keras.preprocessing import image
 import tensorflow as tf
@@ -11,8 +10,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 SIZE=160
-
-logging.basicConfig(level=logging.INFO)
 
 
 UPLOAD_FOLDER = '/static/uploads'
@@ -28,12 +25,10 @@ def home():
 
 @app.route('/upload', methods=['POST'])
 def fileUpload():
-    logger.info("welcome to upload`")
     file = request.files['file']
     filename = secure_filename(file.filename)
     img = (Image.open(file))
     img = np.asarray(img)
-    logger.info(img.shape)
     Image.fromarray(img).save('./test.png')
     img = cv2.imread('./test.png',1)
     img = cv2.resize(img, (SIZE, SIZE))
@@ -41,13 +36,14 @@ def fileUpload():
     img = tf.keras.preprocessing.image.img_to_array(img)
     img = np.clip(model.predict(img.reshape(1,SIZE, SIZE,3)),0.0,1.0).reshape(SIZE, SIZE,3)
     img = (img * 255 / np.max(img)).astype('uint8')
-    Image.fromarray(img).save('./test.png')
-    return send_file('./test.png', mimetype='image/png')
+    img = Image.fromarray(img)
+    img.save('./test.png')
+    return send_file('test.png', mimetype='image/png', as_attachment=True)
 
 
 
 if __name__ == "__main__":
     app.secret_key = os.urandom(24)
-    app.run(debug=True,host="0.0.0.0",use_reloader=False)
+    app.run()
 
 CORS(app, expose_headers='Authorization')
